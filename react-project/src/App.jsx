7eb5e749a449c7f654f431d26095ca2c3c716e49
Lydia Doza <lydia.doza@intel.com>
2025-06-18 14:30:17 -0700

    finish video 2.4

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,13 +1,25 @@
 import "./App.css";
 
-let language = "JavaScript";
-let moon = "🌙";
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to My React App</h1>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <h1>
-      Hello {language.toUpperCase()} {moon}
-    </h1>
+    <div>
+      <Header />
+      <main>
+        <p>This is a simple React application.</p>
+        <p>Feel free to explore and modify the code!</p>
+      </main>
+      <footer>
+        <p>&copy; 2023 My React App</p>
+      </footer>
+    </div>
   );
 }
 
