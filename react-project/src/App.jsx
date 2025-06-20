9f57955eee3e1c81e8ce1a6b07c4c601897fcaf6
Lydia Doza <lydia.doza@intel.com>
2025-06-20 12:17:48 -0700

    finish 3.4 use fragment

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import chef from "./images/chef.jpg";
+import React from "react";
 
 function Header({ name, year }) {
   return (
@@ -23,22 +24,25 @@ const dishObjects = items.map((dish, i) => ({
 
 function Main({ dishes }) {
   return (
-    <main>
-      <img
-        src={chef}
-        //src="https://github.com/Lydia-at-Intel.png"
-        alt="a photo of a smiling Chef owner"
-        height={200}
-        className="chef-image"
-      />
-      <ul>
-        {dishes.map((dish) => (
-          <li key={dish.id} style={{ listStyleType: "none" }}>
-            {dish.title}
-          </li>
-        ))}
-      </ul>
-    </main>
+    <React.Fragment>
+      <h2>Today's Special Dishes</h2>
+      <main>
+        <img
+          src={chef}
+          //src="https://github.com/Lydia-at-Intel.png"
+          alt="a photo of a smiling Chef owner"
+          height={200}
+          className="chef-image"
+        />
+        <ul>
+          {dishes.map((dish) => (
+            <li key={dish.id} style={{ listStyleType: "none" }}>
+              {dish.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </React.Fragment>
   );
 }
 
