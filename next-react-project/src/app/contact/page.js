16294d2000901aea1149d4256a78d631d2c4804a
Lydia Doza <lydia.doza@intel.com>
2025-06-23 18:55:53 -0700

    finish 5.4

---
+++ next-react-project/src/app/contact/page.js
@@ -0,0 +1,8 @@
+export default function MountainPage() {
+  return (
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <h1>Mountain Page</h1>
+      <p>This is the mountain page content.</p>
+    </main>
+  );
+}
