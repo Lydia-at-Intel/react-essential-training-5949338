db171b7fa2d88c18d6e6e3e3ed6121ad4384d99e
Lydia Doza <lydia.doza@intel.com>
2025-06-23 18:47:52 -0700

    5.3 add hotels page

---
+++ next-react-project/src/app/hotels/page.js
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main>
+      <div>
+        <h1>Hotels Page</h1>
+        <p>This is the hotels page content.</p>
+      </div>
+    </main>
+  );
+}
