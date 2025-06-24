96bc9f63c2b986003e78e79af119e43ab2beda57
Lydia Doza <lydia.doza@intel.com>
2025-06-23 19:20:16 -0700

    add async function and call to external api

--- next-react-project/src/app/mountain/page.js
+++ next-react-project/src/app/mountain/page.js
@@ -1,8 +1,14 @@
-export default function MountainPage() {
+async function getData() {
+  const res = await fetch("https://snowtooth-api-rest.fly.dev");
+  return res.json();
+}
+
+export default async function MountainPage() {
+  const data = await getData();
   return (
     <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       <h1>Mountain Page</h1>
-      <p>This is the mountain page content.</p>
+      <p>{JSON.stringify(data)}</p>
     </main>
   );
 }
