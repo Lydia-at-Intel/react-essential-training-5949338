ed573a7c0bcc37f88091d661e938c74463598eb7
Lydia Doza <lydia.doza@intel.com>
2025-06-23 19:27:24 -0700

    finish 6.1

--- next-react-project/src/app/mountain/page.js
+++ next-react-project/src/app/mountain/page.js
@@ -8,6 +8,22 @@ export default async function MountainPage() {
   return (
     <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
       <h1>Mountain Page</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>lift name</th>
+            <th>current status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((lift) => (
+            <tr key={lift.id}>
+              <td>{lift.name}</td>
+              <td>{lift.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <p>{JSON.stringify(data)}</p>
     </main>
   );
